248ec7cea357874b8300c01b82f5d957ef649da9
Linus Torvalds <torvalds@linux-foundation.org>
2022-09-28 22:44:41 +0200

    feat: finish environment printer

--- programme.py
+++ programme.py
@@ -1,8 +1,8 @@
 import os
 
 def print_env(env):
-    """This method displays the environment variables"""
-    pass
+    for k, v in env.items():
+        print("%s: %s" % (k, v))
 
 if __name__ == "__main__":
     print('ENV variables:')
