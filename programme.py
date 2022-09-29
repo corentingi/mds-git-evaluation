1fd33f643a4d85fb9b2ddb9338ba7dc6cc1ba8be
Corentin Gitton <corentin.gitton@gmail.com>
2022-09-29 12:59:28 +0200

    feat(python): rebase variable function

--- programme.py
+++ programme.py
@@ -3,6 +3,9 @@ import os
 def print_env(env):
     for k, v in env.items():
         print("%s: %s" % (k, v))
+    """This method displays the environment variables"""
 
 if __name__ == "__main__":
+    print('ENV variables:')
+    print("---")
     print_env(os.environ)
