24d07ec26adc12e2917bbd41ceee2781e83c3f6f
bachami <55685706+bachami@users.noreply.github.com>
2022-09-29 16:19:30 +0200

    feat(python): add method in script and remove readme and note.txt

--- programme.py
+++ programme.py
@@ -4,7 +4,6 @@ def print_env(env):
     """This method displays the environment variables"""
         for k, v in env.items():
         print("%s: %s" % (k, v))
-        pass
 
 if __name__ == "__main__":
     print('ENV variables:')
